6387c41d313e6749316c1b58b60ad7f1c20e7737
kinnamanfalk929 <kinnamanfalk929@gmail.com>
2025-03-05 22:56:06 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,8 @@
+  <?php
+   $math_homework = array();
+   $math_homework[0] = "2 + 3";
+   $math_homework[1] = "5 - 2";
+   $math_homework[2] = "7 * 4";
+   $math_homework[3] = "9 / 3";
+   return $math_homework;
+?>
