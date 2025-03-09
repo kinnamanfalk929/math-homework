9297744adceb18945f70f982cdb9f609561d418e
kinnamanfalk929 <kinnamanfalk929@gmail.com>
2025-03-09 06:26:20 +0000

    Add 5.php

---
+++ src/5.php
@@ -0,0 +1,11 @@
+<?php
+// Function to calculate the area of a circle
+function circleArea($radius) {
+  return pi() * $radius * $radius;
+}
+
+// Function to calculate the circumference of a circle
+function circleCircumference($radius) {
+  return 2 * pi() * $radius;
+}
+?>
