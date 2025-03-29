ffa973e5de81bb7066a551b1d9308cd86d6f9fc1
kinnamanfalk929 <kinnamanfalk929@gmail.com>
2025-03-29 19:34:05 +0000

    Add 21.php

---
+++ src/21.php
@@ -0,0 +1,11 @@
+<?php 
+  $studentName = "John Doe";
+  $classNumber = 12345;
+  $currentGrade = 90.8;
+
+  if ($currentGrade >= 90) {
+    echo "Congratulations, you have passed the course.";
+  } else {
+    echo "Sorry, your current grade is not yet passable.";
+  }
+?>
