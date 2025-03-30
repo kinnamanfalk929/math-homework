0e763506b2414fbbbd750d1535a9e3b637dd6a55
kinnamanfalk929 <kinnamanfalk929@gmail.com>
2025-03-30 04:10:45 +0000

    Add 22.php

---
+++ src/22.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants
+$var1 = "This is variable 1";
+$var2 = "This is variable 2";
+
+// Check if variables are defined
+if (isset($var1) && isset($var2)) {
+    // Use both variables
+} else {
+    echo "One or more variables are undefined.";
+}
+?>
