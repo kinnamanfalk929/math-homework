b5f0ecf3ffe1c1ef9ae5f42b4618ad2d7f08fe03
kinnamanfalk929 <kinnamanfalk929@gmail.com>
2025-04-07 03:22:52 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,19 @@
+<?php
+// Example PHP code to demonstrate basic arithmetic operations
+
+// Adding 2 and 3
+$sum = 2 + 3;
+
+// Subtracting 1 from the sum
+$result = $sum - 1;
+
+echo "The result of adding 2 and 3 is: $result";
+
+// Multiplying 5 by 2
+$multiplied_value = 5 * 2;
+
+// Adding 4 to the multiplied value
+$total_sum = $multiplied_value + 4;
+
+echo "The total sum is: $total_sum";
+?>
