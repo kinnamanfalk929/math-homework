cc237601d5cde3a93e4bb6e6410e158ac75bb785
kinnamanfalk929 <kinnamanfalk929@gmail.com>
2025-04-26 05:50:26 +0000

    Add 63.php

---
+++ src/63.php
@@ -0,0 +1,29 @@
+<?php
+// Example PHP code for solving mathematical equations
+
+$x = 5;
+$y = 2;
+
+// Addition
+$result_addition = $x + $y;
+
+// Subtraction
+$result_subtraction = $x - $y;
+
+// Multiplication
+$result_multiplication = $x * $y;
+
+// Division
+$result_division = $x / $y;
+
+// Modulo
+$result_modulo = $x % $y;
+
+// Exponentiation
+$result_exponentiation = pow($x, $y);
+
+// Square root
+$result_square_root = sqrt($x);
+
+// Factorial
+$result_factorial = factorial($x);
