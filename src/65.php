106f53e654a5ddf2fe88b7c633da20f5e72b54d4
kinnamanfalk929 <kinnamanfalk929@gmail.com>
2025-05-18 23:16:40 +0000

    Add 65.php

---
+++ src/65.php
@@ -0,0 +1,14 @@
+<?php
+// Define variables and constants
+$var1 = "value1";
+$const1 = 42;
+
+// Check if variable is defined
+if (isset($var1)) {
+    // Do something with $var1
+    echo "Variable $var1 is defined.";
+} else {
+    // Handle the case when $var1 is not set or undefined
+    echo "Variable $var1 is not set or undefined.";
+}
+?>
